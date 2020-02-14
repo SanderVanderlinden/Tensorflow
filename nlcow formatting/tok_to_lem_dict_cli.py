eb70766f8ccfe1885eb4029d65458a94133f0355
Sander Vanderlinden <sander.vanderlinden@student.ucll.be>
2020-02-14 13:47:01 +0100

    scripts

---
+++ nlcow formatting/tok_to_lem_dict_cli.py
@@ -0,0 +1,46 @@
+'''
+HOW TO USE?
+python tok_to_lem_dict_cli.py tok_file lem_file outputfile
+'''
+
+import sys
+
+def tok_to_lem_dict(input_file1, input_file2, output_file):
+    contents1 = split_in_words(input_file1)
+    contents2 = split_in_words(input_file2)
+
+    output = dict()
+
+    line_index = 0
+    for line in contents1:
+        print(len(line))
+        word_index = 0
+        for word in line:
+            #print(word_index)
+            if word_index == (len(line) - 1):
+                None #doe niks
+            elif word_index == 0 or word_index == (len(line) - 2):
+                None #doe niks
+            else:
+                output[word] = contents2[line_index][word_index]
+            word_index += 1
+        line_index += 1
+
+    with open(output_file, 'w') as file:
+        file.write(str(output))
+
+
+def split_in_words(input_file) :
+    with open(input_file, 'r') as file:
+        contents = file.read()
+
+    contents = contents.split("\n")
+
+    line_index = 0
+    for line in contents:
+        contents[line_index] = line.split(" ")
+        line_index += 1
+
+    return contents
+
+tok_to_lem_dict(sys.argv[1], sys.argv[2], sys.argv[3])
