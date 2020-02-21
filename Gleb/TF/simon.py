8d050c65614fcf818c3304249664fa40b7b3b396
Gleb S <gleb0406@hotmail.com>
2020-02-21 10:16:15 +0100

    semi code voor glove -> embeddinglayer

---
+++ Gleb/TF/simon.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+from keras.layers import Embedding
+
+
+GLOVE_DIR = 'mapwaarglovezit'
+word_index = []
+EMBEDDING_DIM = 100
+MAX_SEQUENCE_LENGTH = 15
+
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+#Note that we set trainable=False to prevent the weights from being updated during training.
+
+embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
